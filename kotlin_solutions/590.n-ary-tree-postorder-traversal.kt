47a6ca58e0f229779d70800ea0993b9c7c5cd47e
aqwertyuiop48 <aqwertyuiop48@hotmail.com>
2026-02-14 12:34:41 +0000

    .

--- kotlin_solutions/590.n-ary-tree-postorder-traversal.kt
+++ kotlin_solutions/590.n-ary-tree-postorder-traversal.kt
@@ -4,5 +4,5 @@
  * [590] N-ary Tree Postorder Traversal
  */
 
-class Solution {fun postorder(root: Node?): List<Int> = root?.let { mutableListOf<Int>().also { postOrder -> ArrayDeque<Node>().apply { add(it) }.let { stack -> while (stack.isNotEmpty()) postOrder.add(stack.last().`val`).also { stack.removeLast().children.forEach { child -> child?.let { stack.add(it) } } } } }.reversed() } ?: emptyList()}
+class Solution{fun postorder(root: Node?): List<Int> = root?.let { mutableListOf<Int>().also { postOrder -> ArrayDeque<Node>().apply { add(it) }.let { stack -> while (stack.isNotEmpty()) postOrder.add(stack.last().`val`).also { stack.removeLast().children.forEach { child -> child?.let { stack.add(it) } } } } }.reversed() } ?: emptyList()}
 
