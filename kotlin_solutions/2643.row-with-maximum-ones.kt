6d9df58415e757880c4b1d4375d42bcc5c9d983f
aqwertyuiop48 <aqwertyuiop48@hotmail.com>
2026-02-15 06:35:05 +0000

    .

--- kotlin_solutions/2643.row-with-maximum-ones.kt
+++ kotlin_solutions/2643.row-with-maximum-ones.kt
@@ -4,5 +4,5 @@
  * [2643] Row With Maximum Ones
  */
 
-class Solution {fun rowAndMaximumOnes(mat: Array<IntArray>): IntArray =mat.mapIndexed { i, row -> intArrayOf(i, row.sum()) }.maxBy { it[1] }}
+class Solution{fun rowAndMaximumOnes(mat: Array<IntArray>): IntArray =mat.mapIndexed { i, row -> intArrayOf(i, row.sum()) }.maxBy { it[1] }}
 
