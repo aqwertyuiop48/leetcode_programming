e89e675f385115a3b66e79afa4ab9ce9432b9585
aqwertyuiop48 <aqwertyuiop48@hotmail.com>
2026-04-14 19:40:18 +0000

    .

--- java_solutions/521.longest-uncommon-subsequence-i.java
+++ java_solutions/521.longest-uncommon-subsequence-i.java
@@ -5,11 +5,7 @@
  */
 
 class Solution {
-    public int[][] transpose(int[][] matrix) {
-        return IntStream.range(0, matrix[0].length)
-            .mapToObj(c -> IntStream.range(0, matrix.length)
-                .map(r -> matrix[r][c])
-                .toArray())
-            .toArray(int[][]::new);
+    public int findLUSlength(String a, String b) {
+        return a.equals(b)? -1 : Math.max(a.length(),b.length());
     }
 }
